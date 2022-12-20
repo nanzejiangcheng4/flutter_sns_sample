05dc4101db33368b79a33ac7feff6f09c92e6982
nanzejiangcheng4 <nanzejiangcheng4@gmail.com>
2022-12-20 22:08:00 +0900

    LoginPageのUI実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sns_sample/view/screen.dart';
+import 'package:flutter_sns_sample/view/start_up/login_page.dart';
 import 'package:flutter_sns_sample/view/time_line/time_line_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Screen(),
+      home: LoginPage(),
     );
   }
 }
